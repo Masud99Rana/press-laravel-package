befa5aaebf855b4d32dd9aa0d741671298d1c363
Masud99Rana <Masudrana917775@gmail.com>
2020-01-06 00:33:38 +0600

    markdown parser added

---
+++ src/MarkdownParser.php
@@ -0,0 +1,20 @@
+<?php
+namespace masud\Press;
+use Parsedown;
+class MarkdownParser
+{
+    /**
+     * Given a markdown string, it will pass back a parsed string.
+     *
+     * @param $string
+     *
+     * @return string
+     */
+    public static function parse($string)
+    {   
+        // $parsedown = new Parsedown;
+        // return $parsedown->text($string);
+        
+        return Parsedown::instance()->text($string);
+    }
+}
---
+++ tests/Feature/MarkdownTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace masud\Press\Tests;
+
+use Orchestra\Testbench\TestCase;
+use Parsedown;
+use masud\Press\MarkdownParser;
+
+class MarkdownTest extends TestCase{
+
+	/** @test */
+    public function simple_markdown_is_parsed()
+    {
+        $this->assertEquals('<h1>Heading</h1>', MarkdownParser::parse('# Heading'));
+    }
+}
